42af07259182c16a610d6b01a0dc0a9a37fc0f47
bmcgovern14 <bmcgovern14@msjastudent.org>
2013-07-15 15:37:50 -0500

    shuffle atom types

--- newAtomicNumber.py
+++
@@ -1,7 +0,0 @@
-def newAtomicNumber(atoms):
-	numbers=atoms.get_atomic_numbers()
-	random.shuffle(numbers)
-	atoms.set_atomic_numbers(numbers)
-	return atoms.get_atomic_numbers()
-
-newAtomicNumber(atoms)
