10468697df6dfab54e2bfb381369839219b42d96
conwayje <conwayje@gmail.com>
2013-07-15 20:46:46 -0400

    Update simulated_annealing.py

--- simulated_annealing.py
+++ simulated_annealing.py
@@ -14,6 +14,7 @@ import ase
 import tsase
 from qsc import QSC
 from numpy import *
+from numpy import random as nprandom
 from copy import deepcopy
 from ase import units, io, Atoms
 from tsase import md 
