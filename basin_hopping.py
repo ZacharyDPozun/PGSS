bc3c10e6ee7fef60faa836b44a3b1251c5f0be37
Ngame989 <ngame989@comcast.net>
2013-07-18 16:04:39 -0400

    Update basin_hopping.py

--- basin_hopping.py
+++ basin_hopping.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 #####################################################
-# The first line must be that line.                 # 
+# The first line must be that line.                 #
 # It tells the computer this is a python script     #
 #                                                   #
 # The next thing we do is import all of the         #
@@ -37,7 +37,7 @@ from mpl_toolkits.mplot3d import Axes3D
 ## chosen from a Gaussian distribution in X Y and Z
 ##
 
-def shell_move(inAtom,atomIndex):  
+def shell_move(inAtom,atomIndex):
   #  we're going to be changing the position of atomIndex inside inAtom
   #  make sure that you remove any crazy outliers before you do this
   #  or else it'll just make a bunch more outliers, which is a poor idea
@@ -45,32 +45,31 @@ def shell_move(inAtom,atomIndex):
   try:
     inCOM = inAtom.get_center_of_mass()
     inDistances = distanceCenter(inAtom)
-    
     ninetyNinthRadius = stats.scoreatpercentile(inDistances,99)
-    ninetyFifthRadius = stats.scoreatpercentile(inDistances,95) 
+    ninetyFifthRadius = stats.scoreatpercentile(inDistances,95)
     outerFourRadius = ninetyNinthRadius - ninetyFifthRadius
-    
+
     randomNewRadius = random.normal( (ninetyNinthRadius+ninetyFifthRadius)/2 , (ninetyNinthRadius - ninetyFifthRadius)/2 )
     xFromCenter = random.uniform(0,randomNewRadius)
     randomNewRadius = ((randomNewRadius**2) - (xFromCenter**2))**0.5
     yFromCenter = random.uniform(0,randomNewRadius)
     zFromCenter = ((randomNewRadius**2) - (yFromCenter**2))**0.5
-    
+
     newXPosition = inCOM[0] + plusOrMinus()*xFromCenter
     newYPosition = inCOM[1] + plusOrMinus()*yFromCenter
     newZPosition = inCOM[2] + plusOrMinus()*zFromCenter
-    
+
     positionArray = inAtom.get_positions()
     positionArray[atomIndex] = (newXPosition,newYPosition,newZPosition)
     inAtom.set_positions(positionArray)
-    
+
     return inAtom
-    
+
   except IndexError:
     print "The index of the atom you wanted to move is too high or too low."
     print "Please check your function call of shell_move(a,b)"
     print "-Jeff"
-    
+
 def jolt(inAtom,magnitude):
   """Moves all the atoms in inAtom by a Gaussian distribution
   with mean of 0 and StdDev of magnitude.
@@ -83,14 +82,13 @@ def jolt(inAtom,magnitude):
     posList[x][2] = posList[x][2] + random.normal(0,magnitude)
   inAtom.set_positions(posList)
   return inAtom
-  
+
 def HighEnergyMove(atoms):
     for cntr in range (len(atoms)):
         if (atoms[cntr].getNeighboringAtoms(.5) > 3):
             decideSingleMove()
         elif (atoms[cntr].getNeighboringAtoms(.5) <= 3):
             shake(atoms)
-            
 def ball_move(inAtom,atomIndex):
   """takes an atom defined by atomIndex inside of inAtom
   and moves it somewhere within the core of the atom randomly.
@@ -125,39 +123,56 @@ def ball_move(inAtom,atomIndex):
     print "Please check your function call of ball_move(a,b)"
     print "-Jeff"
 
-def rattleAtoms(atoms):
+def shake(atoms):
         seed = random.randint(100)
         atoms.rattle(stdev=0.5,seed=seed)
         return atoms
 
+def switchAtoms(atoms):
+        numbers = atoms.get_atomic_numbers()
+        random.shuffle(numbers)
+        atoms.set_atomic_numbers(numbers)
+        return atoms
+
+def smallSwitchAtoms(atoms):
+ numbers = atoms.get_atomic_numbers()
+        firstAtom = random.randint(0,len(numbers))
+        secondAtom = random.randint(0,len(numbers))
+        if numbers[firstAtom] == numbers[secondAtom]:
+                smallSwitchAtoms(atoms)
+        else:
+                numbers[firstAtom],numbers[secondAtom] = numbers[secondAtom],numbers[firstAtom]
+                atoms.set_atomic_numbers(numbers)
+                return atoms
+
 def moveAtoms(numbertomove,atoms):
-	totalAtoms = len(atoms)
-	positions = atoms.get_positions()
-	for i in range(numbertomove):
-		atomtomove = random.randint(totalAtoms) # choose a random atom number
-		displacementX = random.normal(scale = 1.0)
-		displacementY = random.normal(scale = 1.0)
-		displacementZ = random.normal(scale = 1.0)
-		positions[atomtomove,0] += displacementX
-		positions[atomtomove,1] += displacementY
-		positions[atomtomove,2] += displacementZ
-	atoms.set_positions(positions)
-	print atoms.get_potential_energy()
-	return atoms
-	
+        totalAtoms = len(atoms)
+        positions = atoms.get_positions()
+        for i in range(numbertomove):
+                atomtomove = random.randint(totalAtoms) # choose a random atom number
+                displacementX = random.normal(scale = 1.0)
+                displacementY = random.normal(scale = 1.0)
+                displacementZ = random.normal(scale = 1.0)
+                positions[atomtomove,0] += displacementX
+                positions[atomtomove,1] += displacementY
+                positions[atomtomove,2] += displacementZ
+        atoms.set_positions(positions)
+        print atoms.get_potential_energy()
+        return atoms
+
 def preventExplosions(atoms):
-	positions = atoms.get_positions()
-	for i in range(len(atoms)):
-		for j in range(len(atoms)):
-			distance = numpy.sqrt(numpy.dot((positions[j] - positions[i]),(positions[j] - positions[i])))
-			if ((distance < 1.5) and (i != j)):
-				print i, j, distance
-				unitDirection = (positions[j] - positions[i]) / distance
-				positions[j] += unitDirection
-	atoms.set_positions(positions)
-	return atoms
-	
-	
+        positions = atoms.get_positions()
+        for i in range(len(atoms)):
+                for j in range(len(atoms)):
+                        distance = numpy.sqrt(numpy.dot((positions[j] - positions[i]),(positions[j] - positions[i])))
+                        if ((distance < 1.5) and (i != j)):
+                                print i, j, distance
+                                unitDirection = (positions[j] - positions[i]) / distance
+                                positions[j] += unitDirection
+        atoms.set_positions(positions)
+        return atoms
+
+
 ########################################################
 # Now we can actually run our basin hopping            #
 # First we need to define our initial variables        #
@@ -171,7 +186,7 @@ sinceLastFind = 0
 
 ###########################################################
 # Here is the main body of the code. We'll load our atoms #
-# object and attach a calculator (QSC). 
+# object and attach a calculator (QSC).
 ###########################################################
 
 ##atoms = makeBimetallic('POSCAR',100,78,79,0.5)
@@ -180,38 +195,40 @@ sinceLastFind = 0
 ##minimaList = PickleTrajectory('Pt75Au25.traj',mode='a')
 
 ##for i in range(NMoves):
-##	numbertomove = random.randint(len(atoms))
-##	atoms = moveAtoms(numbertomove,atoms)
-##	atoms.center()
-##	atoms = preventExplosions(atoms)
-##	# do a last optimization of the structure
-##	dyn = FIRE(atoms)
-##	dyn.run()
-##	newEnergy = atoms.get_potential_energy()
-##	if (newEnergy < bestEnergy):
-##		bestEnergy = newEnergy
-##		line = str(totalMinimaFound) + "  " + str(atoms.get_potential_energy()) + "  " + str(i) +"\n"
-##		print line
-##		f = open('EnergyList.txt','a')
-##		f.write(line)
-##		f.close()
-##		minimaList.write(atoms)
-##		totalMinimaFound += 1
-##		sinceLastFind = 0
-##	elif (sinceLastFind < 200): # if we haven't found a new minimum in 200 tries, start over
-##		atoms = ase.io.read('POSCAR')
-##		calc = QSC()
-##		atoms.set_calculator(calc)
-##		atoms = makeBimetallic(atoms,79,78,0.25)
-##		sinceLastFind = 0
-		
+##      numbertomove = random.randint(len(atoms))
+##      atoms = moveAtoms(numbertomove,atoms)
+##      atoms.center()
+##
+##      atoms = preventExplosions(atoms)
+##      # do a last optimization of the structure
+##      dyn = FIRE(atoms)
+##      dyn.run()
+##      newEnergy = atoms.get_potential_energy()
+##      if (newEnergy < bestEnergy):
+##              bestEnergy = newEnergy
+##              line = str(totalMinimaFound) + "  " + str(atoms.get_potential_energy()) + "  " + str(i) +"\n"
+##              print line
+##              f = open('EnergyList.txt','a')
+##              f.write(line)
+##              f.close()
+##              minimaList.write(atoms)
+##              totalMinimaFound += 1
+##              sinceLastFind = 0
+##      elif (sinceLastFind < 200): # if we haven't found a new minimum in 200 tries, start over
+##              atoms = ase.io.read('POSCAR')
+##              calc = QSC()
+##              atoms.set_calculator(calc)
+##              atoms = makeBimetallic(atoms,79,78,0.25)
+##              sinceLastFind = 0
 
 
 
-##minimaList.close()		
+
+##minimaList.close()
 ##minimaList = PickleTrajectory('Pt75Au25.traj',mode='r')
 
 ##atomslist = [atom for atom in minimaList]
 ##ase.io.write('movie.xyz',atomslist,format='xyz') # write a movie file of our dynamics
 
 ##minimaList.close()
+
