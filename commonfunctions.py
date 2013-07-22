47ddcff1ebce7fd8b2e8da57fc0e92acdee7159b
conwayje <conwayje@gmail.com>
2013-07-22 16:20:11 -0400

    Update commonfunctions.py

--- commonfunctions.py
+++ commonfunctions.py
@@ -7,7 +7,6 @@ from ase import io, optimize, md, units, Atoms
 from ase.optimize import FIRE
 from ase.io.trajectory import PickleTrajectory
 from ase.md import VelocityVerlet
-from commonfunctions import nearlySphericalAtom
 
 
 def create_sample_atom(inNumberOfAtoms,inAtomType):
