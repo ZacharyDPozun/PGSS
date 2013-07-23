919676981fafcf333c5b9df789c6f2826ec08ad7
conwayje <conwayje@gmail.com>
2013-07-23 00:54:35 -0400

    Update basin_hopping.py

--- basin_hopping.py
+++ basin_hopping.py
@@ -65,8 +65,11 @@ def mainBasinLoop(symbol1, symbol2, elementN1, elementN2, numberOfType1, numberO
     bigKickResults.append(shake(baseAtom))
     bigKickResults.append(switchAtoms(baseAtom))
     
+    calc = QSC()
+    
   print "The same already??"
   for x in range(len(bigKickResults)):
+    bigKickResults[x].set_calculator(calc)
     print bigKickResults[x].get_potential_energy()
 
   for x in range(len(bigKickResults)):
