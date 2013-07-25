b1f6da6abdf6fe71325fda1e551c61dfab660304
conwayje <conwayje@gmail.com>
2013-07-25 15:55:08 -0400

    Update returnFinalBestAtom.py

--- returnFinalBestAtom.py
+++ returnFinalBestAtom.py
@@ -18,8 +18,8 @@ def returnFinalBestAtom(trajectoryFile):
   candidates = PickleTrajectory(str(trajectoryFile),mode='r')
   molecules = [molecule for molecule in candidates]
   potentialEnergyList = [molecule.get_potential_energy() for molecule in molecules]
-  minimumPotenialEnergy = min(potentialEnergyList)
-  indexOfMinimumPotentialEnergy = potentialEnergyList.index(minimumPotenialEnergy)
+  minimumPotentialEnergy = min(potentialEnergyList)
+  indexOfMinimumPotentialEnergy = potentialEnergyList.index(minimumPotentialEnergy)
   bestAtom = molecules[indexOfMinimumPotentialEnergy].copy()
   
   print "Best PE Follows"
