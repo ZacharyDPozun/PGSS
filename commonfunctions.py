678eb6bbad5d8d38194185def59ca477368ace62
conwayje <conwayje@gmail.com>
2013-07-29 12:10:17 -0400

    yup

--- commonfunctions.py
+++ commonfunctions.py
@@ -8,7 +8,7 @@ from ase.optimize import FIRE
 from ase.io.trajectory import PickleTrajectory
 from ase.md import VelocityVerlet
 
-change = "meaningless change"
+change2 = "another change"
 
 def create_sample_atom(inNumberOfAtoms,inAtomType):
   """Creates a mostly spherical atom with inNumberOfAtoms atoms
