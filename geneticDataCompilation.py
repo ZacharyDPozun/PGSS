33c480e3ee76db53efd1b0b8e16cdc9fdf35bba5
conwayje <conwayje@gmail.com>
2013-07-30 20:39:07 -0400

    yup

--- geneticDataCompilation.py
+++ geneticDataCompilation.py
@@ -2,16 +2,21 @@
 
 import os
 
-os.chdir('/home/zach/Genetic')
-
 compiledEnergies = open('fullOutputGenetic.txt',mode = 'a')
+
+os.chdir('/home/zach/Genetic')
 
 subdirectories = os.listdir(os.getcwd())
 subdirectories.remove('submit_hopping.py') #remove *????
 for alphaValue in subdirectories[:94]:
   os.chdir(str(alphaValue))
   readFile = open('Output.txt', mode = 'r')
-  for line in readFile:
+  dataLines = [line for line in readFile if len(line)==6]
+  averageLines = [line for line in readFile if len(line)==7]
+  allLines = dataLines+averageLines
+  for line in allLines:
+    line.append(alphaValue)
+  for line in allLines:
     compiledEnergies.writelines(str(line))
   os.chdir('..')
 
