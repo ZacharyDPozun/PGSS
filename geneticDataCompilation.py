4215cdd06c15b0ba0a451760360171351203cf25
conwayje <conwayje@gmail.com>
2013-07-31 09:10:24 -0400

    94 lines

--- geneticDataCompilation.py
+++ geneticDataCompilation.py
@@ -8,7 +8,7 @@ os.chdir('/home/zach/Genetic')
 
 subdirectories = os.listdir(os.getcwd())
 subdirectories.remove('submit_hopping.py') #remove *????
-for alphaValue in subdirectories[:94]:
+for alphaValue in subdirectories:
   os.chdir(str(alphaValue))
   readFile = open('Output.txt', mode = 'r')
   for line in readFile:
