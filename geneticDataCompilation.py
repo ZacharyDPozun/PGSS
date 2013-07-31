846530f4dc0c5e10b4faf8b66c3fa2daa915e822
conwayje <conwayje@gmail.com>
2013-07-30 20:41:24 -0400

    ok

--- geneticDataCompilation.py
+++ geneticDataCompilation.py
@@ -11,8 +11,8 @@ subdirectories.remove('submit_hopping.py') #remove *????
 for alphaValue in subdirectories[:94]:
   os.chdir(str(alphaValue))
   readFile = open('Output.txt', mode = 'r')
-  dataLines = [line for line in readFile if len(line)==6]
-  averageLines = [line for line in readFile if len(line)==7]
+  dataLines = [line for line in readFile if len(line.split())==6]
+  averageLines = [line for line in readFile if len(line.split())==7]
   allLines = dataLines+averageLines
   for line in allLines:
     line.append(alphaValue)
